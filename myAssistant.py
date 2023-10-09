43855b3de9052a2c1cff53c99abafb06c9e70a6c
code-with-santanu <santanupal003@gmail.com>
2023-10-09 13:33:51 +0530

    (greet, date, time, offline) func added

---
+++ myAssistant.py
@@ -0,0 +1,41 @@
+from speak import *
+
+import datetime
+
+
+
+def greet():
+    speak("welcome back mr. Santanu")
+    hour = datetime.datetime.now().hour
+    if (hour >= 4 and hour < 12):
+        speak("Good morning sir")
+    elif (hour >= 12 and hour < 17):
+        speak("Good afternoon sir")
+    elif (hour >= 17 and hour < 20):
+        speak("Good evening sir")
+    else:
+        speak("Good night sir")
+
+    speak("alfred at your service. how can I help you?")
+
+
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak("the current time is")
+    speak(Time)
+
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    day = int(datetime.datetime.now().day)
+    speak("today's date is ")
+    speak(day)
+    speak(month)
+    speak(year)
+
+
+def offline():
+    speak('thank you')
+    speak('going offline')
+    quit()
