7cf5db11a06d81deaf35618397402c743d12ddfd
code-with-santanu <santanupal003@gmail.com>
2023-10-10 12:56:33 +0530

    wikipedia search added

--- main.py
+++ main.py
@@ -18,7 +18,22 @@ if __name__ == "__main__":
 
         if 'time' in query:
             time()
-        if 'date' in query:
+
+        elif 'date' in query:
             date()
-        if 'offline' in query:
+
+        elif ('wikipedia' in query) or ('search' in query):
+            speak('searching.....')
+            query = query.replace('wikipedia', "")
+            query = query.replace('search', "")
+            if 'in details' in query:
+                query = query.replace('in details', "")
+                search(query, 20)
+            elif 'in short' in query:
+                query = query.replace('in short', "")
+                search(query, 5)
+            else:
+                search(query, 2)
+
+        elif 'offline' in query:
             offline()
